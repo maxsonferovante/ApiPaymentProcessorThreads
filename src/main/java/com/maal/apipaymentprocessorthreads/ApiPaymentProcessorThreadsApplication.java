6e4d1e32fd8a644e8195d4ee8db6b19d5c58e168
Maxson Almeida <maxsonferovante@gmail.com>
2025-07-29 20:15:41 -0300

    init commit

---
+++ src/main/java/com/maal/apipaymentprocessorthreads/ApiPaymentProcessorThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.maal.apipaymentprocessorthreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPaymentProcessorThreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPaymentProcessorThreadsApplication.class, args);
+    }
+
+}
